402659993b74e0934620f0849f38b4d06a892671
Irina Evsei <irinaevsei.ie@gmail.com>
2017-09-26 15:27:52 +0300

    new commit

--- Lab_3/src/com/main/java/bsu/epam_labs/lab_3/Main.java
+++ Lab_3/src/com/main/java/bsu/epam_labs/lab_3/Main.java
@@ -1,5 +1,6 @@
 package com.main.java.bsu.epam_labs.lab_3;
 
+import java.util.Scanner;
 
 /**
  * Created by irisha on 26.09.2017.
@@ -7,6 +8,23 @@ package com.main.java.bsu.epam_labs.lab_3;
 public class Main {
     public static void main(String[] args) {
 
+        Scanner in = new Scanner(System.in);
+        int a,b;
 
+        System.out.println("Введите а ");
+        a = in.nextInt();
+
+        System.out.println("Введите b ");
+        b = in.nextInt();
+
+        System.out.println(" a+b = " + (a+b));
+        System.out.println(" a-b = " + (a-b));
+        System.out.println(" a*b = " + (a*b));
+        if(b==0) {
+            System.out.println("ERROR!");
+        }
+        else {
+            System.out.println(" a/b = " + (a/b));
+        }
     }
 }
